dde1b2a7d7b6632afd8905036da6e62811152690
Nishu Singh <nsingh2878@sdsu.edu>
2021-10-07 16:08:43 -0700

    Implementation of Homework 3

---
+++ Homework3.py
@@ -0,0 +1,102 @@
+import sys
+
+from pyspark import SparkConf, SparkContext, StorageLevel
+from pyspark.sql import SparkSession
+
+from batting_avg_transform import BattingAverageTransform
+
+
+def main():
+
+    # Building spark session
+    conf = SparkConf().set("spark.jars", "./jars/mariadb-java-client-2.7.4.jar")
+    SparkContext(conf=conf)
+    spark = (
+        SparkSession.builder.master("local[*]")
+        .config("spark.sql.debug.maxToStringFields", 128)
+        .getOrCreate()
+    )
+    user = "root"  # pragma: allowlist secret
+    password = "abc123"  # pragma: allowlist secret
+
+    # Dataframe for batter_counts table
+    batter_dataframe = (
+        spark.read.format("jdbc")
+        .options(
+            url="jdbc:mysql://localhost:3306/newbaseball",
+            driver="org.mariadb.jdbc.Driver",
+            dbtable="batter_counts",
+            user=user,
+            password=password,
+        )
+        .load()
+    )
+    batter_dataframe.createOrReplaceTempView("batter_counts")
+    batter_dataframe.persist(StorageLevel.MEMORY_AND_DISK)
+
+    # Dataframe for game table
+    game_dataframe = (
+        spark.read.format("jdbc")
+        .options(
+            url="jdbc:mysql://localhost:3306/newbaseball",
+            driver="org.mariadb.jdbc.Driver",
+            dbtable="game",
+            user=user,
+            password=password,
+        )
+        .load()
+    )
+    game_dataframe.createOrReplaceTempView("game")
+    game_dataframe.persist(StorageLevel.MEMORY_AND_DISK)
+
+    # Dataframe rolling lookup
+    rolling_lookup_df = (
+        spark.read.format("jdbc")
+        .options(
+            url="jdbc:mysql://localhost:3306/newbaseball",
+            driver="org.mariadb.jdbc.Driver",
+            dbtable="(SELECT g.game_id , local_date , batter, atBat , Hit \
+                                FROM batter_counts as bc JOIN game as g ON g.game_id = bc.game_id \
+                                WHERE atBat > 0 \
+                                ORDER BY batter, local_date) batter_avg_temp",
+            user=user,
+            password=password,
+        )
+        .load()
+    )
+    # rolling_lookup_df.show()
+    rolling_lookup_df.createOrReplaceTempView("batter_avg_temp")
+    rolling_lookup_df.persist(StorageLevel.MEMORY_AND_DISK)
+
+    # Sql query to calculate the total hits and total atBat.
+    rolling_sum = spark.sql(
+        """ SELECT
+                bart1.batter
+                , bart1.game_id
+                , bart1.local_date
+                , SUM(bart2.Hit) AS Total_Hit
+                , SUM(bart2.atBat) As Total_atBat
+                FROM batter_avg_temp bart1 JOIN
+                batter_avg_temp bart2 ON
+                bart1.batter = bart2.batter AND
+                bart2.local_date BETWEEN DATE_SUB(bart1.local_date, 100) AND
+                bart1.local_date
+                GROUP BY
+                bart1.batter, bart1.game_id, bart1.local_date
+                """
+    )
+    rolling_lookup_df.createOrReplaceTempView("batter_avg_temp")
+    rolling_lookup_df.persist(StorageLevel.MEMORY_AND_DISK)
+
+    # Custom transformation for calculation of rolling average.
+    batting_avg_transform = BattingAverageTransform(
+        inputCols=["Total_Hit", "Total_atBat"], outputCol="Batting_Average"
+    )
+    rolling_average = batting_avg_transform.transform(rolling_sum)
+    print("+----------------Batting Average for Rolling over 100 Days---------------+")
+    # Final Results
+    rolling_average.show(n=100, truncate=False)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ batting_avg_transform.py
@@ -0,0 +1,34 @@
+from pyspark import keyword_only
+from pyspark.ml import Transformer
+from pyspark.ml.param.shared import HasInputCols, HasOutputCol
+from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
+from pyspark.sql.functions import round
+
+
+class BattingAverageTransform(
+    Transformer,
+    HasInputCols,
+    HasOutputCol,
+    DefaultParamsReadable,
+    DefaultParamsWritable,
+):
+    @keyword_only
+    def __init__(self, inputCols=None, outputCol=None):
+        super(BattingAverageTransform, self).__init__()
+        kwargs = self._input_kwargs
+        self.setParams(**kwargs)
+        return
+
+    @keyword_only
+    def setParams(self, inputCols=None, outputCol=None):
+        kwargs = self._input_kwargs
+        return self._set(**kwargs)
+
+    def _transform(self, dataset):
+        input_cols = self.getInputCols()
+        output_col = self.getOutputCol()
+
+        dataset = dataset.withColumn(
+            output_col, round(dataset[input_cols[0]] / dataset[input_cols[1]], 3)
+        )
+        return dataset
