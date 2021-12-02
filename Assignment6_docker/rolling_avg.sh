3f22db2488ca117cdd2e505717b1feb1e54da0be
Nishu Singh <nsingh2878@sdsu.edu>
2021-12-01 23:45:34 -0800

    testing

--- Assignment6_docker/rolling_avg.sh
+++ Assignment6_docker/rolling_avg.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# for sql shell scripting, took reference from stackoverflow
+#https://stackoverflow.com/questions/7616520/how-to-execute-a-sql-script-from-bash
 
 echo "checking and loading baseball database"
 if ! mysql -h bda696_mariadb -u root -pabc123 -e 'USE baseball'; then
@@ -6,13 +8,13 @@ if ! mysql -h bda696_mariadb -u root -pabc123 -e 'USE baseball'; then
   echo "baseball database does not exists"
   mysql -h bda696_mariadb -u root -pabc123 -e create baseball
   echo "loading database..."
-  mysql -u root -pabc123 -h bda696_mariadb --database=baseball < /sql/baseball.sql
+  mysql -u root -pabc123 -h bda696_mariadb --database=baseball < /app/sql/baseball.sql
 
   else
-    echo "baseball does exists"
+    echo "baseball exists"
   fi
 
-mysql -h bda696_mariadb -u root -pabc123 baseball < /sql/Homework2.sql
+mysql -h bda696_mariadb -u root -pabc123 baseball < /app/sql/Homework2.sql
 mysql -h bda696_mariadb -u root -pabc123 baseball -e '
  SELECT * FROM batter_avg_rolling; '> /app/output/rolling_table.txt
  echo "result data stored in result folder"
