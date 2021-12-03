e68fd196031176d82398ea93ffd4a1885814db45
Nishu Singh <nsingh2878@sdsu.edu>
2021-12-02 17:06:23 -0800

    updated Implementation of Assignment6

--- Assignment6_docker/rolling_avg.sh
+++ Assignment6_docker/rolling_avg.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-# for sql shell scripting, took reference from stackoverflow
-#https://stackoverflow.com/questions/7616520/how-to-execute-a-sql-script-from-bash
 
+# 1. for sql shell scripting, took reference from stackoverflow
+#    https://stackoverflow.com/questions/7616520/how-to-execute-a-sql-script-from-bash
+#2. using sleep, to start the bda696_mariadb service completely.
+#   Otherwise you might get error "can not connect mysql_service".
+#   Ref https://stackoverflow.com/questions/31746182/docker-compose-wait-for-container-x-before-starting-y
+#3. Reusing homework2.sql with little changes.
+
+
+sleep 5
 echo "checking and loading baseball database"
 if ! mysql -h bda696_mariadb -u root -pabc123 -e 'USE baseball'; then
 
@@ -11,7 +18,7 @@ if ! mysql -h bda696_mariadb -u root -pabc123 -e 'USE baseball'; then
   mysql -u root -pabc123 -h bda696_mariadb --database=baseball < /app/sql/baseball.sql
 
   else
-    echo "baseball exists"
+    echo "baseball database exists"
   fi
 
 mysql -h bda696_mariadb -u root -pabc123 baseball < /app/sql/Homework2.sql
